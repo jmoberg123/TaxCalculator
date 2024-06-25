0cb729dc09dd6f0dc00455a6d5c2da2a8536758c
joel.moberg_tradera <joel.moberg@tradera.com>
2024-06-25 22:35:46 +0200

    Fix formatting, remove unused usings

--- TaxCalculator.Core/TaxCalculator.cs
+++ TaxCalculator.Core/TaxCalculator.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-
-//The focus should be on clean, simple and easy to read code 
+﻿//The focus should be on clean, simple and easy to read code
 //Everything but the public interface may be changed
 namespace TaxCalculatorInterviewTests
 {
@@ -12,7 +8,6 @@ namespace TaxCalculatorInterviewTests
     /// It also allows our client to remotely set new, custom tax rates.
     /// Finally, it allows the fetching of tax rate information for a specific commodity and point in time.
     /// TODO: We know there are a few bugs in the code below, since the calculations look messed up every now and then.
-    ///       There are also a number of things that have to be implemented.
     /// </summary>
     public class TaxCalculator : ITaxCalculator
     {
@@ -27,7 +22,6 @@ namespace TaxCalculatorInterviewTests
             return _taxRateRepository.GetTaxRateByCommodity(commodity).Rate;
         }
 
-
         /// <summary>
         /// This method allows the client to remotely set new custom tax rates.
         /// When they do, we save the commodity/rate information as well as the UTC timestamp of when it was done.
@@ -37,7 +31,6 @@ namespace TaxCalculatorInterviewTests
         {
             _customTaxRateRepository.Add(commodity, rate);
         }
-
 
         /// <summary>
         /// Gets the tax rate that is active for a specific point in time (in UTC).
