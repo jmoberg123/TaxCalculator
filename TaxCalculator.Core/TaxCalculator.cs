2da26afee1d248a168f0eebe4acaf9663a6ad126
joel.moberg_tradera <joel.moberg@tradera.com>
2024-06-25 19:25:56 +0200

    Break out Commodity enum to its own file

--- TaxCalculator.Core/TaxCalculator.cs
+++ TaxCalculator.Core/TaxCalculator.cs
@@ -75,18 +75,5 @@ namespace TaxCalculatorInterviewTests
             //TODO: implement
             throw new NotImplementedException();
         }
-
-    }
-
-    public enum Commodity
-    {
-        //PLEASE NOTE: THESE ARE THE ACTUAL TAX RATES THAT SHOULD APPLY, WE JUST GOT THEM FROM THE CLIENT!
-        Default,            //25%
-        Alcohol,            //25%
-        Food,               //12%
-        FoodServices,       //12%
-        Literature,         //6%
-        Transport,          //6%
-        CulturalServices    //6%
     }
 }
