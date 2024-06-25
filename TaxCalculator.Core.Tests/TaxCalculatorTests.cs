8731e78e0cfe98903c67f6c5463cbdadfffa14b4
joel.moberg_tradera <joel.moberg@tradera.com>
2024-06-25 21:22:43 +0200

    Add TaxRateRepository with method to get TaxRate by commodity

---
+++ TaxCalculator.Core.Tests/TaxCalculatorTests.cs
@@ -0,0 +1,34 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+using TaxCalculatorInterviewTests;
+
+namespace TaxCalculator.Core.Tests;
+using TaxCalculator = TaxCalculatorInterviewTests.TaxCalculator;
+
+public class TaxCalculatorTests
+{
+
+    private TaxCalculator _taxCalculator;
+
+    [SetUp]
+    public void SetUp()
+    {
+        _taxCalculator = new TaxCalculator();
+    }
+
+    [TestCase(Commodity.Default, 0.25)]
+    [TestCase(Commodity.Alcohol, 0.25)]
+    [TestCase(Commodity.Food, 0.12)]
+    [TestCase(Commodity.FoodServices, 0.12)]
+    [TestCase(Commodity.Literature, 0.6)]
+    [TestCase(Commodity.Transport, 0.6)]
+    [TestCase(Commodity.CulturalServices, 0.6)]
+    public void GetStandardTaxRate_ShouldReturnExpectedRates(Commodity commodity, double expectedRate)
+    {
+        //Act
+        var rate = _taxCalculator.GetStandardTaxRate(commodity);
+
+        //Assert
+        rate.Should().Be(expectedRate);
+    }
+}
---
+++ TaxCalculator.Core/TaxRateRepository.cs
@@ -0,0 +1,23 @@
+﻿namespace TaxCalculatorInterviewTests;
+
+public class TaxRateRepository
+{
+    private static TaxRate DefaultTaxRate => new(Commodity.Default, 0.25);
+
+    private readonly IReadOnlyList<TaxRate> _taxRates = new List<TaxRate>
+    {
+        DefaultTaxRate,
+        new(Commodity.Alcohol, 0.25),
+        new(Commodity.Food, 0.12),
+        new(Commodity.FoodServices, 0.12),
+        new(Commodity.Literature, 0.6),
+        new(Commodity.Transport, 0.6),
+        new(Commodity.CulturalServices, 0.6),
+    };
+
+    public TaxRate GetTaxRateByCommodity(Commodity commodity)
+    {
+        return _taxRates.FirstOrDefault(x => x.Commodity == commodity)
+            ?? DefaultTaxRate;
+    }
+}
